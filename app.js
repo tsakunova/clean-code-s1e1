7eab6eaeeb96c38b270c08fa64cbe7cd4afc0013
Tatsiana Tsakunova <tv.tsakunova@gmail.com>
2021-11-28 12:49:51 +0300

    refactor: used semantic tags according to rule 1.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="delete btn";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="delete_img";
     deleteButton.appendChild(deleteButtonImg);
 
 
